2f6fe80d4de65fe8acbe0489befad9cbd348b204
Syed Jawad Shah <syedjawadshah00@gmail.com>
2024-04-17 19:23:36 +0500

    Frontend is about to be pushed

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Home from "./home/home";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Courses from "./courses/courses";
+import Signup from "./components/Signup";
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./components/AuthProvider";
+
+function App() {
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
+  return (
+    <>
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/course"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Toaster />
+      </div>
+    </>
+  );
+}
+
+export default App;
